f771c6c1a4e73780fbee29b80ba9c76ff63e0124
Nasonov Dmitriy <onlydmitriy@gmail.com>
2021-04-14 01:48:19 +0300

    hw5

--- app/src/main/java/com/example/androidhw_5/SettingActivity.java
+++ app/src/main/java/com/example/androidhw_5/SettingActivity.java
@@ -1,14 +1,50 @@
 package com.example.androidhw_5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class SettingActivity extends AppCompatActivity {
 
+    private Switch myswitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.DarkThem);
+        }
+        else setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        myswitch=(Switch)findViewById(R.id.myswitch);
+        if (AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            myswitch.setChecked(true);
+        }
+        myswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    restartApp();
+                } else
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                restartApp();
+            }
+        });
+    }
+    public void restartApp (){
+    Intent i = new Intent(getApplicationContext(), SettingActivity.class);
+    startActivity(i);
+    finish();
+    }
+    public void exitSettings(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
